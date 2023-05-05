1b1beebfedc2f99350bed161a7a05beb6fd1f393
Skerz <claudequarante@gmail.com>
2023-05-05 18:05:24 +0200

    v2 complete

---
+++ src/app/immatriculation/immatriculation-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+import { ImmatriculationComponent } from "./immatriculation.component";
+@Component({
+selector : 'immatriculation-list',
+templateUrl :'./immatriculation-list.component.html',
+})
+export class immatriculationList{
+}
